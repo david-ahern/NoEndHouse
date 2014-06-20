6e2daa63c5fac9ebdc90576cc0995314a15d4eef
David Ahern <david-ahern@hotmail.co.uk>
2014-06-21 00:15:23 +0100

    Dialogue Inspector window

--- Assets/Editor/DialogueTriggerGUI.cs
+++ Assets/Editor/DialogueTriggerGUI.cs
@@ -2,6 +2,8 @@
 using UnityEditor;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System;
 
 [CustomEditor(typeof(DialogueTrigger))]
 public class DialogueTriggerGUI : Editor
@@ -15,6 +17,7 @@ public class DialogueTriggerGUI : Editor
     static bool ShowDialogue = true;
 
     static List<bool> DialogueFoldouts = new List<bool>();
+    static List<bool> OptionsFoldouts = new List<bool>();
 
     public override void OnInspectorGUI()
     {
@@ -30,8 +33,12 @@ public class DialogueTriggerGUI : Editor
         Texture DownArrow = GUIIconEditor.GetIcon("Down Icon");
 
         Repaint();
-
-        GUILayout.Label("Hello world");
+        EditorGUILayout.Space();
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.Label("Use Trigger:");
+        Target.Trigger = EditorGUILayout.Toggle(Target.Trigger);
+        EditorGUILayout.EndHorizontal();
+        EditorGUILayout.Space();
 
         GUIStyle FoldoutStyle = new GUIStyle(EditorStyles.foldout);
         FoldoutStyle.fontStyle = FontStyle.Bold;
@@ -46,34 +53,84 @@ public class DialogueTriggerGUI : Editor
         if (DialogueFoldouts.Count < Target.Dialogues.Count)
             for (int i = DialogueFoldouts.Count; i < Target.Dialogues.Count; i++)
                 DialogueFoldouts.Add(false);
-        if (ShowDialogue)
-        {
-            foreach (Dialogue dialogue in Target.Dialogues)
-            {
-                EditorGUILayout.BeginHorizontal();
-                DialogueFoldouts[Target.Dialogues.IndexOf(dialogue)] = EditorGUILayout.Foldout(DialogueFoldouts[Target.Dialogues.IndexOf(dialogue)], (dialogue.Clip ? dialogue.Clip.name : "No Clip"));
-                if (GUILayout.Button(UpArrow, GUILayout.Width(20), GUILayout.Height(20))) MoveUp(Target.Dialogues.IndexOf(dialogue));
-                if (GUILayout.Button(DownArrow, GUILayout.Width(20), GUILayout.Height(20))) MoveDown(Target.Dialogues.IndexOf(dialogue));
-                if (GUILayout.Button(RemoveButton, GUILayout.Width(20), GUILayout.Height(20))) removeDialogue = dialogue;
-                EditorGUILayout.EndHorizontal();
+        if (OptionsFoldouts.Count < Target.Dialogues.Count)
+            for (int i = OptionsFoldouts.Count; i < Target.Dialogues.Count; i++)
+                OptionsFoldouts.Add(false);
 
-                if (DialogueFoldouts[Target.Dialogues.IndexOf(dialogue)])
+                if (ShowDialogue)
                 {
-                    dialogue.Clip = (AudioClip)EditorGUILayout.ObjectField(dialogue.Clip, typeof(AudioClip));
-                    dialogue.Subtitle = GUILayout.TextArea(dialogue.Subtitle);
+                    foreach (Dialogue dialogue in Target.Dialogues)
+                    {
+                        EditorGUILayout.BeginHorizontal();
+                        GUILayout.Space(20);
+                        DialogueFoldouts[Target.Dialogues.IndexOf(dialogue)] = EditorGUILayout.Foldout(DialogueFoldouts[Target.Dialogues.IndexOf(dialogue)], (dialogue.Clip ? dialogue.Clip.name : "No Clip"));
+                        if (GUILayout.Button(UpArrow, GUILayout.Width(20), GUILayout.Height(10))) MoveUp(Target.Dialogues.IndexOf(dialogue));
+                        if (GUILayout.Button(DownArrow, GUILayout.Width(20), GUILayout.Height(10))) MoveDown(Target.Dialogues.IndexOf(dialogue));
+                        if (GUILayout.Button(RemoveButton, GUILayout.Width(20), GUILayout.Height(20))) removeDialogue = dialogue;
+                        EditorGUILayout.EndHorizontal();
+
+                        if (DialogueFoldouts[Target.Dialogues.IndexOf(dialogue)])
+                        {
+                            EditorGUILayout.BeginHorizontal();
+                            GUILayout.Space(40);
+                            OptionsFoldouts[Target.Dialogues.IndexOf(dialogue)] = EditorGUILayout.Foldout(OptionsFoldouts[Target.Dialogues.IndexOf(dialogue)], "Audio Options:");
+                            EditorGUILayout.EndHorizontal();
+
+                            if (OptionsFoldouts[Target.Dialogues.IndexOf(dialogue)])
+                            {
+                                EditorGUILayout.BeginHorizontal();
+                                GUILayout.Space(40);
+                                GUILayout.Label("Clip:", GUILayout.Width(70));
+                                dialogue.Clip = (AudioClip)EditorGUILayout.ObjectField(dialogue.Clip, typeof(AudioClip));
+                                EditorGUILayout.EndHorizontal();
+
+                                EditorGUILayout.BeginHorizontal();
+                                GUILayout.Space(40);
+                                GUILayout.Label("Volume:", GUILayout.Width(70));
+                                dialogue.Volume = GUILayout.HorizontalSlider(dialogue.Volume, 0, 1);
+                                GUILayout.Space(20);
+                                EditorGUILayout.EndHorizontal();
+                                EditorGUILayout.BeginHorizontal();
+                                GUILayout.Space(40);
+                                GUILayout.Label("Delay:", GUILayout.Width(70));
+                                string temp = GUILayout.TextField(dialogue.Delay.ToString(), GUILayout.Width(50));
+
+                                temp = Regex.Replace(temp, @"[^a-zA-Z0-9 ]", "");
+                                if (temp == "")
+                                    dialogue.Delay = 0;
+                                else
+                                    dialogue.Delay = System.Convert.ToSingle(temp);
+                                EditorGUILayout.EndHorizontal();
+                                EditorGUILayout.Space();
+                            }
+
+                            EditorGUILayout.BeginHorizontal();
+                            GUILayout.Space(40); 
+                            GUILayout.Label("Subtitle:");
+                            EditorGUILayout.EndHorizontal();
+                            EditorGUILayout.BeginHorizontal();
+                            GUILayout.Space(40);
+                            dialogue.Subtitle = GUILayout.TextArea(dialogue.Subtitle);
+                            EditorGUILayout.EndHorizontal();
+                            EditorGUILayout.Space();
+                            GUILayout.Box(GUIContent.none, new GUILayoutOption[] { GUILayout.ExpandWidth(true), GUILayout.Height(1) });
+                            EditorGUILayout.Space();
+                        }
+                    }
                 }
-            }
-        }
 
         if (removeDialogue != null)
         {
             DialogueFoldouts.RemoveAt(Target.Dialogues.IndexOf(removeDialogue));
             Target.Dialogues.Remove(removeDialogue);
         }
-
+        EditorGUILayout.Space();
         GUILayout.BeginHorizontal();
+        GUILayout.Label("Skip Queue:");
         Target.SkipQueue = EditorGUILayout.Toggle(Target.SkipQueue);
+        GUILayout.Label("Stop Current:");
         Target.StopCurrent = EditorGUILayout.Toggle(Target.StopCurrent);
+        GUILayout.Label("Clear Queue:");
         Target.ClearQueue = EditorGUILayout.Toggle(Target.ClearQueue);
         GUILayout.EndHorizontal();
         EditorUtility.SetDirty(Target);
@@ -84,8 +141,15 @@ public class DialogueTriggerGUI : Editor
         if (index == 0) return;
 
         var temp = Target.Dialogues[index - 1];
+        bool tempb = DialogueFoldouts[index - 1];
+        bool tempc = OptionsFoldouts[index - 1];
         Target.Dialogues[index - 1] = Target.Dialogues[index];
+        DialogueFoldouts[index - 1] = DialogueFoldouts[index];
+        OptionsFoldouts[index - 1] = OptionsFoldouts[index];
         Target.Dialogues[index] = temp;
+        DialogueFoldouts[index] = tempb;
+        OptionsFoldouts[index] = tempc;
+
     }
 
     void MoveDown(int index)
@@ -93,7 +157,13 @@ public class DialogueTriggerGUI : Editor
         if (index == Target.Dialogues.Count - 1) return;
 
         var temp = Target.Dialogues[index + 1];
+        bool tempb = DialogueFoldouts[index + 1];
+        bool tempc = OptionsFoldouts[index + 1];
         Target.Dialogues[index + 1] = Target.Dialogues[index];
+        DialogueFoldouts[index + 1] = DialogueFoldouts[index];
+        OptionsFoldouts[index + 1] = OptionsFoldouts[index];
         Target.Dialogues[index] = temp;
+        DialogueFoldouts[index] = tempb;
+        OptionsFoldouts[index] = tempc;
     }
 }
